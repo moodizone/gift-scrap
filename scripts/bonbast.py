408543159e1b40c16ee09fdec047bbc8ed224632
moodi <moodizone@gmail.com>
2024-12-06 00:49:56 +0330

    🚧 try to overcome anti-bot mechanism

--- scripts/bonbast.py
+++ scripts/bonbast.py
@@ -6,66 +6,88 @@ from utils.index import generate_dates, save_file
 url = "https://bonbast.com/archive"
 
 
-async def scrap_page(page: Page, date: str):
-    date_picker = await page.query_selector("form.form-inline input[type='text']")
-    form = await page.query_selector("form.form-inline")
+async def go_to_date(page: Page, date: str):
+    input = await page.query_selector("form.form-inline input[type='text']")
+    day = date.split("-")[-1]
 
-    if date_picker and form:
-        await page.fill("form.form-inline input[type='text']", date, timeout=2000)
-        await page.locator("form.form-inline").click()
-        time.sleep(3)
-        tables = await page.query_selector_all("table")
+    # removing leading 0
+    p_day = f"{int(day)}"
 
-        if tables:
-            info = {}
-            for index, table in enumerate(tables):
+    if input:
+        await input.focus()
+        await input.fill(date)
 
-                # currency table
-                if index < 2:
-                    rows = await table.query_selector_all("tr:not(.info)")
+        # close datepicker
+        await input.press("Enter", delay=100)
 
-                    if rows:
-                        for index, row in enumerate(rows):
-                            tds = await row.query_selector_all("td")
+        # submit form
+        await input.press("Enter", delay=100)
+        return
 
-                            # [0] --> Code
-                            # [1] --> Currency
-                            # [2] --> Sell (this number is important)
-                            # [3] --> Buy
-                            if tds:
-                                code = await tds[0].text_content()
-                                sell = await tds[2].text_content()
-                                # only pick the valid prices
-                                if (
-                                    sell != None
-                                    and code != None
-                                    and sell not in ["", "None", "0"]
-                                ):
-                                    info[code] = sell
-                # coin table
-                elif index == 2:
-                    rows = await table.query_selector_all("tr:not(.info)")
+        # wait for calendar to pop-up
+        # await page.wait_for_selector("#ui-datepicker-div", timeout=5000)
+        # tbody = await page.query_selector("table.ui-datepicker-calendar tbody")
+        # if tbody:
+        #     day = await tbody.query_selector(f"td:has-text('{p_day}')")
+        #     if day:
+        #         print(await day.inner_html())
+        #         await day.click()
+        #         return
+    raise ValueError("❌ Can not find form controllers")
 
-                    if rows:
-                        for index, row in enumerate(rows):
-                            tds = await row.query_selector_all("td")
 
-                            # [0] --> Coin
-                            # [1] --> Sell (this number is important)
-                            # [2] --> Buy
-                            if tds:
-                                code = await tds[0].text_content()
-                                sell = await tds[1].text_content()
-                                # only pick the valid prices
-                                if (
-                                    sell != None
-                                    and code != None
-                                    and sell not in ["", "None", "0"]
-                                ):
-                                    info[code] = sell
-            return info
-    else:
-        print("❌ Can not find form controllers")
+async def scrap_page(page: Page):
+
+    tables = await page.query_selector_all("table")
+
+    if tables:
+        info = {}
+        for index, table in enumerate(tables):
+
+            # currency table
+            if index < 2:
+                rows = await table.query_selector_all("tr:not(.info)")
+
+                if rows:
+                    for index, row in enumerate(rows):
+                        tds = await row.query_selector_all("td")
+
+                        # [0] --> Code
+                        # [1] --> Currency
+                        # [2] --> Sell (this number is important)
+                        # [3] --> Buy
+                        if tds:
+                            code = await tds[0].text_content()
+                            sell = await tds[2].text_content()
+                            # only pick the valid prices
+                            if (
+                                sell != None
+                                and code != None
+                                and sell not in ["", "None", "0"]
+                            ):
+                                info[code] = sell
+            # coin table
+            elif index == 2:
+                rows = await table.query_selector_all("tr:not(.info)")
+
+                if rows:
+                    for index, row in enumerate(rows):
+                        tds = await row.query_selector_all("td")
+
+                        # [0] --> Coin
+                        # [1] --> Sell (this number is important)
+                        # [2] --> Buy
+                        if tds:
+                            code = await tds[0].text_content()
+                            sell = await tds[1].text_content()
+                            # only pick the valid prices
+                            if (
+                                sell != None
+                                and code != None
+                                and sell not in ["", "None", "0"]
+                            ):
+                                info[code] = sell
+        return info
 
 
 async def main():
@@ -78,13 +100,32 @@ async def main():
 
         data = {}
         await page.goto(url)
-        start_date = "2024-10-20"
-        end_date = "2024-11-20"
+        start_date = "2023-01-01"
+        end_date = "2023-01-31"
         time.sleep(5)
 
         year = start_date.split("-")[0]
         for date in generate_dates(start_date, end_date):
-            data[date] = await scrap_page(page, date)
+            attempt = 0
+            while attempt < 3:
+                await go_to_date(page, date)
+                try:
+                    await page.wait_for_selector(
+                        f"em.miladi:has-text('{date} .')", timeout=3000
+                    )
+                except:
+                    attempt += 1
+                    if attempt >= 3:
+                        print(f"Skipped {date} because of error(s)")
+                        continue
+                    # wait before retrying (optional)
+                    # suspends the execution of the current coroutine
+                    # this allows other tasks to run in the meantime without blocking the event loop.
+                    # preventing the script from sending requests too quickly in succession
+                    await asyncio.sleep(2)
+                else:
+                    data[date] = await scrap_page(page)
+                    break
         save_file(data, f"bonbast/{year}")
         await browser.close()
 
